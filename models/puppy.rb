521f52cf144c8062af507fa51e24d871f6cf5689
vicfriedman <victoria.m.friedman@gmail.com>
2015-11-10 16:39:42 -0500

    solution

---
+++ models/puppy.rb
@@ -0,0 +1,10 @@
+class Puppy
+
+  attr_accessor :name, :age, :breed
+
+  def initialize(name, breed, age)
+    @name = name
+    @age = age
+    @breed = breed
+  end
+end
---
+++ spec/sinatra_basic_forms_lab_spec.rb
@@ -0,0 +1,62 @@
+
+require 'pry'
+describe "Puppy Adoption Site" do
+  describe "GET '/'" do
+    before(:each) do
+      get '/'
+    end
+
+    it "returns a 200 status code" do
+      expect(last_response.status).to eq(200)
+    end
+
+    it "renders a welcome" do
+      expect(last_response.body).to include("Welcome to the puppy adoption site!")
+      expect(last_response.body).to include("Click Here To List A Puppy")
+    end
+  end
+
+  describe "GET '/new'" do
+    before(:each) do
+      get '/new'
+    end
+
+    it "returns a 200 status code" do
+      expect(last_response.status).to eq(200)
+    end
+
+    it "renders a new form element on the page" do
+      expect(last_response.body).to include("<form")
+      expect(last_response.body).to include("</form>")
+    end
+
+    it "renders the puppy input fields for name, breed, and age attributes on the page" do
+      # binding.pry
+      expect(last_response.body).to include("name")
+      expect(last_response.body).to include("breed")
+      expect(last_response.body).to include("age")
+    end
+  end
+
+  describe "POST '/puppy'" do
+    before do
+      post '/puppy', {
+          "name"=>"Ian",
+          "breed"=>"Dalmation",
+          "age"=>"6",
+      }
+    end
+
+    it "returns a 200 status code" do
+      expect(last_response.status).to eq(200)
+    end
+
+    it "displays the puppy information upon form submission" do
+      expect(last_response.body).to include("Ian")
+      expect(last_response.body).to include("Dalmation")
+      expect(last_response.body).to include("6")
+    end
+
+  
+  end
+end
