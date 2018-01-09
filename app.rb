ab19b86a0e0161d24c9b384bf53c3157c3f90453
Steven G. Harms <steven.harms@flatironschool.com>
2018-01-09 11:57:54 -0500

    Add passing solution

--- app.rb
+++ app.rb
@@ -2,4 +2,20 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/puppy' do
+    @name = params['name']
+    @age = params['age']
+    @breed = params['breed']
+    erb :display_puppy
+  end
+
+
 end
