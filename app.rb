6b7567a1fc80b38428864928b38db08054a5c153
Cedric David <cedric.david@btopenworld.com>
2018-09-20 00:25:57 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,5 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :index
+  end
 
+  get '/create_puppy' do
+    erb :create_puppy
+  end
+
+  post '/display_puppy' do
+    @puppy = Puppy.new("#{params[:name]}", "#{params[:breed]}", "#{params[:age]}" )
+    erb :display_puppy
+  end
 end
